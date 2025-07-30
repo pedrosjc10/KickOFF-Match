f7e763a194cf8e9481375f6191c696e42c1728af
João Vitor Soares <142361596+joaosoaresm@users.noreply.github.com>
2025-07-29 22:54:05 -0300

    alterando url api.ts front

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from '../services/authServices';
 
 const api = axios.create({
-  baseURL: 'https://tcc.railway.internal',
+  baseURL: 'https://tcc..up.railway.app',
 });
 
 api.interceptors.request.use(
