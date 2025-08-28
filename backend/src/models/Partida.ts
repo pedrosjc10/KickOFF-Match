42fbb24a82c298c8e7e73ed4d5d99f27e6f0cce4
João Vitor Soares <142361596+joaosoaresm@users.noreply.github.com>
2025-08-28 10:35:50 -0300

    Update Partida.ts

--- backend/src/models/Partida.ts
+++ backend/src/models/Partida.ts
@@ -1,15 +1,30 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany, JoinColumn } from "typeorm";
 import { TipoPartida } from "./TipoPartida";
-import { Local } from "./Local"; // Importe a entidade Local
-import { PartidaUsuario } from "./PartidaUsuario"; // Importe a entidade de relacionamento
+import { Local } from "./Local";
+import { PartidaUsuario } from "./PartidaUsuario";
 
-@Entity("partida") // Nome da tabela no banco
+export enum TipoEnum {
+  PRIVADO = 0,
+  PUBLICO = 1,
+}
+
+@Entity("partida")
 export class Partida {
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Column({ type: "smallint" }) // Tipo corrigido para ser compatível com PostgreSQL
-  tipo!: number;
+  @Column({
+    type: "smallint",
+    transformer: {
+      to: (value: "privado" | "publico") => {
+        return value === "privado" ? TipoEnum.PRIVADO : TipoEnum.PUBLICO;
+      },
+      from: (value: number) => {
+        return value === TipoEnum.PRIVADO ? "privado" : "publico";
+      },
+    },
+  })
+  tipo!: "privado" | "publico";
 
   @Column()
   data!: Date;
@@ -20,17 +35,17 @@ export class Partida {
   @Column()
   nome!: string;
 
-  // Relacionamento com a entidade TipoPartida
+  // Relacionamento com TipoPartida
   @ManyToOne(() => TipoPartida)
-  @JoinColumn({ name: 'tipopartida_idtipopartida' }) // <-- Correção: Especifique o nome da coluna de chave estrangeira
+  @JoinColumn({ name: 'tipopartida_idtipopartida' })
   tipoPartida!: TipoPartida;
 
-  // Relacionamento com a entidade Local
+  // Relacionamento com Local
   @ManyToOne(() => Local)
-  @JoinColumn({ name: 'local_id' }) // <-- Correção: Especifique a chave estrangeira para Local
+  @JoinColumn({ name: 'local_id' })
   local!: Local;
 
-  // Relacionamento com a tabela de junção PartidaUsuario
+  // Relacionamento com PartidaUsuario
   @OneToMany(() => PartidaUsuario, partidaUsuario => partidaUsuario.partida)
   partidaUsuarios!: PartidaUsuario[];
 }
