6ec8c148028699f7371f421b7dc0fb70f0221844
João Vitor Soares <joaovitorsoaresdemoraes59@gmail.com>
2025-10-20 18:27:14 -0300

    button css

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/Buttons.css"; // <-- adicione esta linha
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
